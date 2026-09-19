88240685ea3292a05d08844da7dae160e417dcec
Cable <cableplays1912@gmail.com>
2026-09-19 15:48:35 +0300

    add MCS lock

--- MCSLock.java
+++ MCSLock.java
@@ -1,12 +1,48 @@
+import java.util.concurrent.atomic.AtomicReference;
+
 public class MCSLock implements Lock {
+
+    private final AtomicReference<Node> tail = new AtomicReference<>();
+    private final ThreadLocal<Node> myNode = new ThreadLocal<>();
 
     @Override
     public void lock() {
-        // TODO
+        Node node = new Node();
+        myNode.set(node);
+
+        Node pred = tail.getAndSet(node);
+
+        if (pred != null) {
+            node.locked = true; // there is a predecessor so wait
+            pred.next = node; // tell the pred I exist so it can tell me when it's done
+
+            while (node.locked) {
+                // wait until predecessor says I can continue
+                // Thread.onWaitSpin();
+            }
+        }
     }
 
     @Override
     public void unlock() {
-        // TODO
+        Node node = myNode.get();
+
+        if (node.next == null) {
+            // there is no successor
+            if (tail.compareAndSet(node, null))
+                return; // there is actually no successor
+
+            while (node.next == null) {
+                // there is a successor, wait for it to set my node's next
+                // Thread.onWaitSpin();
+            }
+        }
+
+        node.next.locked = false; // tell successor I'm done and it can continue
+    }
+
+    class Node {
+        boolean locked;
+        Node next;
     }
 }
