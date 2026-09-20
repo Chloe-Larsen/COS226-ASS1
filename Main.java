995f1d8f1abcaab1990a66bb69998c842d0bf305
Chloe Larsen <chloelarsen72@gmail.com>
2026-09-20 21:02:33 +0200

    Changing main for testing

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        int numberOfThreads = 0; /* Change */
+        int numberOfThreads = 16;
         int iterations = 200;
 
         Auction auction = new Auction(AuctionUtils.generateItemName());
-        Lock lock = null; /* Add your lock here */
+        Lock lock = new MCSLock();
         Runner runner = new Runner(numberOfThreads, iterations, auction, lock);
         runner.run();
     }
